6498c6558cb7d7dda369dae5b56e00840a6f49d8
Sandun Rathsara <sandun@surge.global>
2023-09-09 11:37:32 +0530

    Global message provider added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Caveat } from "next/font/google"
 import AppContainer from "@/features/AppContainer"
 import { ThemeToggleProvider } from "@/features/AppContainer/theme-context/toggle-theme"
 import { CocktailThemeProvider } from "@/features/AppContainer/theme-context/cocktail-theme-provider"
+import MessageProvider from "@/configs/message-context"
 
 const fonts = Caveat({ subsets: ["latin"] })
 
@@ -11,14 +12,16 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <title>Cocktail</title>
-      <ThemeToggleProvider>
-        <CocktailThemeProvider>
-          <main className={fonts.className}>
-            <AppContainer />
-            <Component {...pageProps} />
-          </main>
-        </CocktailThemeProvider>
-      </ThemeToggleProvider>
+      <MessageProvider>
+        <ThemeToggleProvider>
+          <CocktailThemeProvider>
+            <main className={fonts.className}>
+              <AppContainer />
+              <Component {...pageProps} />
+            </main>
+          </CocktailThemeProvider>
+        </ThemeToggleProvider>
+      </MessageProvider>
     </>
   )
 }
