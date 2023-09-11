4c386aca10d351c5617838ae1ecc00f3997a5381
Sandun Rathsara <sandun@surge.global>
2023-09-11 13:16:03 +0530

    Default theme changed to light

--- src/features/AppContainer/theme-context/toggle-theme.tsx
+++ src/features/AppContainer/theme-context/toggle-theme.tsx
@@ -1,14 +1,14 @@
 import { createContext, FC, PropsWithChildren, useContext, useState } from "react"
 
 const ThemeToggleContext = createContext<{ theme: "dark" | "light"; toggleTheme: () => void }>({
-  theme: "dark",
+  theme: "light",
   toggleTheme: () => {}
 })
 
 export const useThemeToggle = () => useContext(ThemeToggleContext)
 
 export const ThemeToggleProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [theme, setTheme] = useState<"dark" | "light">("dark")
+  const [theme, setTheme] = useState<"dark" | "light">("light")
 
   const toggleTheme = () => {
     setTheme(state => (state === "dark" ? "light" : "dark"))
